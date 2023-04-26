0390a264492cf7915f5a477262afeb8fe611e1ee
doloresgarro <doloresgarro2@gmail.com>
2023-04-26 18:39:30 -0300

    Practica 2, hasta el 9

--- Práctica-2/Ej3P2.asm
+++ Práctica-2/Ej3P2.asm
@@ -1,12 +1,12 @@
 ORG 1000H
-DIML DB 26             ; cant de veces que lo va a hacer porq son 26 letras
+letra DB ?            
 
 ORG 2000H
 
 MOV CL, 41H            ; A
 MOV CH, 61H            ; a
 
-MOV BX, OFFSET DIML    ; la direc que esta en BX contiene lo que voy a imprimir
+MOV BX, OFFSET letra   ; la direc que esta en BX contiene lo que voy a imprimir
 MOV AL, 1              ; cantidad de caracteres que voy a imprimir 
 
 LOOP:MOV [BX], CL      ; a lo que contiene BX le asigno la mayus
@@ -15,7 +15,7 @@ INC CL
 MOV [BX], CH 	     ; a lo que contiene BX le pongo la minus
 INT 7                  ; imprime la minus porq esta en la direc de BX
 INC CH 
-CMP DIML, CH           ; si CH llega a 26 para
+CMP letra, 7AH         ; si CH llega a ser 7AH(z) para
 JNZ LOOP
 
 
