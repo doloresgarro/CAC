41c4b02bc946d1a031e91b0e9e820d37db595774
Dolores Garro <129607981+doloresgarro@users.noreply.github.com>
2023-04-30 18:53:31 -0300

    Update Ej8P3.asm

--- Práctica-3/Ej8P3.asm
+++ Práctica-3/Ej8P3.asm
@@ -1,18 +1,56 @@
+;VERSION INTERRUPCIÓN
+; no anda :((((
+
+PIC EQU 20H
+HAND EQU 40H
+ESTADO EQU 41H
+N_HND EQU 10
+
+ORG 40
+IP_HND DW RUT_HND
+
 ORG 1000H
-MSJ DB "UNIVERSIDAD "
-DB "NACIONAL DE LA PLATA"
-FIN DB ?
+CAR DB ?
+cantCar db 5
+
+ORG 3500H
+
+EST: MOV BX, ESTADO
+
+MOV AL, [BX]
+salta: CMP AL, 81H   ; 1000 0001 --> ult bit en 1: ocupada
+JZ salta
+ret
 
 ORG 3000H
 
-; desde esta subrutina de interrupcion se imprime 
-; RUTINA de atencion a la interrupcion !!!!!!!!!!!!!
 RUT_HND: PUSH AX
 
-MOV AL, [BX]  ; guarda car a imprimir
-OUT HAND, AL  ; lo manda al buffer 
-INC BX        ; avanza en la palabra
-DEC CL        ; disminuye dimL
+
+MOV BX, OFFSET CAR
+MOV CL, 0
+
+loop: MOV AL, [BX] 
+OUT HAND, AL   ; lo manda al buffer
+
+INC BX
+INC CL
+CMP CL, 5
+JNZ loop
+DEC BX    ; porq sino me imprime dos veces la s
+
+
+loop2: PUSH AX 
+PUSH BX
+CALL EST
+POP BX
+POP AX
+
+DEC BX
+MOV AL, [BX] 
+OUT HAND, AL   ; lo manda al buffer
+DEC CL
+JNZ loop2
 
 MOV AL, 20H   ; termina la interrupcion
 OUT PIC, AL 
@@ -22,8 +60,14 @@ IRET
 
 ORG 2000H
 
-MOV BX, OFFSET MSJ
-MOV CL, OFFSET FIN-OFFSET MSJ
+MOV BX, OFFSET CAR
+MOV CL, cantCar
+
+; los pone en memoria
+pp: INT 6
+INC BX
+DEC CL
+JNZ pp
 
 CLI
 
@@ -32,16 +76,15 @@ MOV AL, 0FBH  ; 1111 1011 activa el HANDSHAKE
 OUT PIC+1, AL ; IMR 
 
 MOV AL, N_HND ; ID de la interrup
-OUT PIC+6, AL ; INT 2
+OUT PIC+6, AL ; INT2
+
 
 ; configuracion del HANDSHAKE
 MOV AL, 80H   ; 1000 0000
-OUT HAND+1, AL; primer dig es 1 --> habilita interrupciones
+OUT ESTADO, AL; primer dig es 1 --> habilita interrupciones
 
 STI
 
-LAZO: CMP CL, 0  ; por que esto se queda en bucle sin hacer nada?
-JNZ LAZO
 
 ; deshabilita interrupciones
 IN AL, HAND+1
