3fcaa53255dc8931e2f48eabaef0f05f2ad41c25
Dolores Garro <129607981+doloresgarro@users.noreply.github.com>
2023-05-14 23:49:56 -0300

    Práctica 4 terminada

--- Práctica-4/Ejercicio8.s
+++ Práctica-4/Ejercicio8.s
@@ -12,11 +12,11 @@ res: .word 0
 
 dadd r1, r0, r0    ;
 ld r2, num1(r0)    ; pongo en r2 el num 
-ld r3, num2(r0)	 ; pongo en r3 la cant de veces q lo voy a multiplicar
+ld r3, num2(r0)	   ; pongo en r3 la cant de veces q lo voy a multiplicar
 
 loop: dadd r1, r1, r2 ; al num le sumo el mismo num
 daddi r3, r3, -1      ; disminuyo cant de veces q quiero multiplicar
-;slti r5, r3, 1        ; comp r3 con 1, xq si comparo con 0 me hace una vez mas
+;slti r5, r3, 1       ; comp r3 con 1, xq si comparo con 0 me hace una vez mas
 bnez r3, loop
 
 sd r1, res(r0)        ; almcaceno en memoria el resultado
